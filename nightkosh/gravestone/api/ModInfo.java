9cc5b5d6b7c1770361ec9b335d2d80696ae671e1
NightKosh <nightkosh@gmail.com>
2016-02-09 05:43:30 +0300

    move ModInfo to API

---
+++ nightkosh/gravestone/api/ModInfo.java
@@ -0,0 +1,16 @@
+package nightkosh.gravestone.api;
+
+/**
+ * GraveStone mod
+ *
+ * @author NightKosh
+ * @license Lesser GNU Public License v3 (http://www.gnu.org/licenses/lgpl.html)
+ */
+public class ModInfo {
+
+    public static final String ID = "GraveStone";
+    public static final String NAME = "GraveStone";
+    public static final String VERSION = "3.0.0";
+    public static final String API_VERSION = "1.0.0";
+}
+}
