49a7814a557272d95d136106ea8b49196f876027
NightKosh <nightkosh@gmail.com>
2017-04-22 13:44:07 +0300

    update version

--- nightkosh/gravestone/api/ModInfo.java
+++ nightkosh/gravestone/api/ModInfo.java
@@ -10,6 +10,6 @@ public class ModInfo {
 
     public static final String ID = "GraveStone";
     public static final String NAME = "GraveStone";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String API_VERSION = "1.1.0";
 }
