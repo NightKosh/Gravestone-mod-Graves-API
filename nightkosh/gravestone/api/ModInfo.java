4cfda322e5021c85b28f06f2cb28d0e808c7ff32
NightKosh <nightkosh@gmail.com>
2017-06-24 14:08:19 +0300

    lowcase ID

--- nightkosh/gravestone/api/ModInfo.java
+++ nightkosh/gravestone/api/ModInfo.java
@@ -8,7 +8,7 @@ package nightkosh.gravestone.api;
  */
 public class ModInfo {
 
-    public static final String ID = "GraveStone";
+    public static final String ID = "gravestone";
     public static final String NAME = "GraveStone";
     public static final String VERSION = "1.0.5";
     public static final String API_VERSION = "1.1.0";
